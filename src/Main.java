a25629245962f8484672441dd32b3ac22508f735
Weslley Marques <weslleymarquess101@gmail.com>
2025-06-22 22:04:15 -0300

    BeeCword 1000 - Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        }
+        /*O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+                Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela*/
+
+        System.out.println("Hello World!");
+    }
 }
