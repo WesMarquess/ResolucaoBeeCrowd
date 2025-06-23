16e9a4eae6abeeba90970e1ffbc3fb00d827b375
WesMarquess <weslleymarquess101@gmail.com>
2025-06-22 22:14:46 -0300

    Ex 1000 até 1002

---
+++ src/Ex1001.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex1001 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int x = a + b;
+        System.out.println("X = " + x);
+    }
+}
+
