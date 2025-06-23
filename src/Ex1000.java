64381f757712563dd6c81b515dda3d98d23e2a2d
WesMarquess <weslleymarquess101@gmail.com>
2025-06-22 22:14:46 -0300

    Ex 1000 até 1002

--- src/Ex1000.java
+++ src/Ex1000.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Ex1000 {
     public static void main(String[] args) {
 
         /*O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
